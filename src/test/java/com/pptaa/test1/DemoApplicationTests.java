baf3423e26cc4e6b3e63733d81dfacd985b98eb7
Lee <chobo05@naver.com>
2020-07-06 19:41:50 +0900

    test commit

--- src/test/java/com/pptaa/test1/DemoApplicationTests.java
+++ src/test/java/com/pptaa/test1/DemoApplicationTests.java
@@ -2,12 +2,22 @@ package com.pptaa.test1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
 
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Bean
+	public HttpFirewall allowUrlSemicolonHttpFirewall(){
+		StrictHttpFirewall firewall = new StrictHttpFirewall();
+		firewall.setAllowSemicolon(true);
+		return firewall;
 	}
 
 }
